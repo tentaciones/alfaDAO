6a4e77374329c739858c36354012dafabbc7954e
tentaciones <102796391+tentaciones@users.noreply.github.com>
2023-03-17 15:59:31 +0100

    code cleanup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,4 @@
-import React, { useRef, useState } from "react";
-
-import emailjs from "@emailjs/browser";
+import React from "react";
 
 import { styles } from "../styles";
 
@@ -9,96 +7,8 @@ import { SectionWrapper } from "../hoc";
 import { Link } from "react-router-dom";
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    Name: "",
-    Twitter: "",
-    Discord: "",
-    Message: "",
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
-
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    fetch(
-      "https://script.google.com/macros/s/AKfycbyY1st1avH1f_qafAW9IxGjkA-l-sm37rN0Vw532UXbFVrMD7q-Y82X5E3qixWF7NU/exec",
-      {
-        method: "POST",
-        body: form,
-      }
-    ).then(
-      () => {
-        setLoading(false);
-        alert("Thank you. I will get back to you as soon as possible.");
-
-        setForm({
-          Name: "",
-          Twitter: "",
-          Discord: "",
-          Message: "",
-        });
-      },
-      (error) => {
-        setLoading(false);
-        console.error(error);
-
-        alert("Ahh, something went wrong. Please try again.");
-      }
-    );
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "chideracharles65",
-          from_email: form.email,
-          to_email: "chideracharles654@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
-          setForm({
-            Name: "",
-            Twitter: "",
-            Discord: "",
-            Message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
-  };
-
   return (
-    <div className="px-32">
+    <div className="md:px-32 px-5">
       <div className=" flex flex-col h-[200px] items-center justify-center rounded-2xl bg-black-100">
         <p className={styles.sectionSubText}>Collab Request form</p>
         <button
--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -4,7 +4,7 @@ import { EarthCanvas } from "./canvas";
 
 const Earth = () => {
   return (
-    <div className=" h-[1000px] w-full">
+    <div className=" md:h-[1000px] w-full h-[500px]">
       <EarthCanvas />
     </div>
   );
